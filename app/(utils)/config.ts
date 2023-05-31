6e273f7fc92fbe7489b36023c97141bacb6b4e3c
Klemen Komel <klemen.komel@razum.si>
2023-05-31 19:22:43 +0200

    fix(config): site name & lang

--- app/(utils)/config.ts
+++ app/(utils)/config.ts
@@ -4,11 +4,11 @@ import colors from "tailwindcss/colors";
 type Language = "si" | "en";
 
 export const siteConfig = {
-  name: "Solar Orodja",
+  name: "Solar Tools",
   logo: "/solar-energy.png",
   mainPageUrl: "https://klemen-komel.com",
   mainColor: colors.orange[500],
-  language: "si" as Language,
+  language: "en" as Language,
 };
 
 export const ConfigContext = createContext(siteConfig);
